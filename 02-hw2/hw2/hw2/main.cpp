c73d86993fa02fb290d335a5f668271e9db860db
tong-meow <tongshen930@gmail.com>
2022-01-17 21:31:14 -0700

    Add hw2

---
+++ 02-hw2/hw2/hw2/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  hw2
+//
+//  Created by Tong Shen on 1/17/22.
+//
+#include "cmdline.hpp"
+#include "expr.hpp"
+
+
+int main(int argc, char **argv){
+
+    use_arguments(argc, argv);
+    return 0;
+
+}
