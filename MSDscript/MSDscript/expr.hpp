68c25b753788419d369f72a13fa740319f9cf0a6
tong-meow <tongshen930@gmail.com>
2022-01-19 19:42:25 -0700

    Finished hw3.

--- MSDscript/MSDscript/expr.hpp
+++ MSDscript/MSDscript/expr.hpp
@@ -14,20 +14,28 @@
 // template Expr
 class Expr {
 public:
-    // each subclass must override this function
+    // each subclass must override these functions
     virtual bool equals(Expr *e) = 0;
+    virtual int interp() = 0;
+    virtual bool has_variable() = 0;
+    virtual Expr* subst(std::string s, Expr *e) = 0;
 };
 
 
+// subclass 1: Num
 class Num : public Expr {
 public:
     int val;
     
     Num(int val);
     virtual bool equals(Expr *e);
+    virtual int interp();
+    virtual bool has_variable();
+    virtual Expr* subst(std::string s, Expr *e);
 };
 
 
+// subclass 2: Add
 class Add : public Expr{
 public:
     Expr* lhs;
@@ -35,9 +43,13 @@ public:
     
     Add(Expr* lhs, Expr* rhs);
     virtual bool equals(Expr *e);
+    virtual int interp();
+    virtual bool has_variable();
+    virtual Expr* subst(std::string s, Expr *e);
 };
 
 
+// subclass 3: Mult
 class Mult : public Expr{
 public:
     Expr* lhs;
@@ -45,15 +57,23 @@ public:
     
     Mult(Expr* lhs, Expr* rhs);
     virtual bool equals(Expr *e);
+    virtual int interp();
+    virtual bool has_variable();
+    virtual Expr* subst(std::string s, Expr *e);
 };
 
 
+// subclass 4: Variable
 class Variable : public Expr{
 public:
     std::string str;
     
     Variable(std::string str);
     virtual bool equals(Expr *e);
+    virtual int interp();
+    virtual bool has_variable();
+    virtual Expr* subst(std::string s, Expr *e);
 };
+
 
 #endif /* expr_hpp */
---
+++ MSDscript/MSDscript/tests.hpp
@@ -0,0 +1,15 @@
+//
+//  tests.hpp
+//  MSDscript
+//
+//  Created by Tong Shen on 1/19/22.
+//
+
+#ifndef tests_hpp
+#define tests_hpp
+
+#include <stdio.h>
+#include "catch.hpp"
+
+
+#endif /* tests_hpp */
